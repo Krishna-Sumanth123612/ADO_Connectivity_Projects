7148a8ec7a81fcd2c06dd08473ddf8538d91acf3
Krishna Sumanth <tacit119@gmail.com>
2024-06-24 18:52:54 +0530

    CRUD operations in disconnected approach in ado.net

--- CoreADODisconnectedArchitecture/Controllers/InventoryController.cs
+++ CoreADODisconnectedArchitecture/Controllers/InventoryController.cs
@@ -1,12 +1,76 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CoreADODisconnectedArchitecture.DAL;
+using CoreADODisconnectedArchitecture.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CoreADODisconnectedArchitecture.Controllers
 {
     public class InventoryController : Controller
     {
+        public InventoryAccessLayer inventoryDAL;
+        public InventoryController(IConfiguration configuration)
+        {
+            inventoryDAL = new InventoryAccessLayer(configuration);
+        }
         public IActionResult Index()
         {
+            return View(inventoryDAL.GetInventories());
+        }
+        public IActionResult Create()
+        {
             return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Inventory inventory)
+        {
+            try
+            {
+                inventoryDAL.AddInventory(inventory);
+                return RedirectToAction("Index");
+            }
+            catch (Exception ex)
+            {
+                return View();
+            }
+        }
+        public IActionResult Details(int id)
+        {
+            return View(inventoryDAL.GetInventory(id));
+        }
+
+        public IActionResult Edit(int id)
+        {
+            return View(inventoryDAL.GetInventory(id));
+        }
+        [HttpPost]
+        public IActionResult Edit(int id, Inventory inventory)
+        {
+            try
+            {
+                inventoryDAL.EditInventory(id, inventory);
+                return RedirectToAction("Index");
+            }
+            catch (Exception ex)
+            {
+                return View();
+            }
+        }
+        public IActionResult Delete(int id)
+        {
+            return View(inventoryDAL.GetInventory(id));
+        }
+
+        [HttpPost]
+        public IActionResult Delete(int id,Inventory inventory)
+        {
+            try
+            {
+                inventoryDAL.DeleteInventory(id, inventory);
+                return RedirectToAction("Index");
+            }
+            catch(Exception ex)
+            {
+                return View();
+            }
         }
     }
 }
--- CoreADODisconnectedArchitecture/DAL/InventoryAccessLayer.cs
+++ CoreADODisconnectedArchitecture/DAL/InventoryAccessLayer.cs
@@ -1,32 +1,123 @@
 ﻿using CoreADODisconnectedArchitecture.Models;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace CoreADODisconnectedArchitecture.DAL
 {
     public class InventoryAccessLayer : IInventoryAccessLayer
     {
+        public IConfiguration Configuration { get; }
+        public InventoryAccessLayer(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
 
         public IEnumerable<Inventory> GetInventories()
         {
-            throw new NotImplementedException();
+            List<Inventory> inventories = new List<Inventory>();
+            string connectionString = Configuration["ConnectionStrings:DefaultConnection"];
+            SqlConnection sqlConnection = new SqlConnection(connectionString);
+            string query = "Select * from Inventory";
+            SqlDataAdapter dataAdapter = new SqlDataAdapter(query, sqlConnection);
+            DataTable dataTable = new DataTable();
+            dataAdapter.Fill(dataTable);
+            foreach (DataRow dataRow in dataTable.Rows)
+            {
+                Inventory inventory = new Inventory();
+                inventory.Id = Convert.ToInt32(dataRow[0]);
+                inventory.Name = dataRow[1].ToString();
+                inventory.Price = Convert.ToInt64(dataRow[2]);
+                inventory.Quantity = Convert.ToInt32(dataRow[3]);
+                inventories.Add(inventory);
+            }
+            return inventories;
         }
         public void AddInventory(Inventory inventory)
         {
-            throw new NotImplementedException();
+            string connectionString = Configuration["ConnectionStrings:DefaultConnection"];
+            SqlConnection sqlConnection = new SqlConnection(connectionString);
+            string query = "Select * from Inventory";
+            SqlDataAdapter dataAdapter = new SqlDataAdapter(query, sqlConnection);
+            SqlCommandBuilder command = new SqlCommandBuilder(dataAdapter);
+            DataTable dataTable = new DataTable();
+            dataAdapter.Fill(dataTable);
+            DataRow dataRow = dataTable.NewRow();
+            dataRow[1] = inventory.Name;
+            dataRow[2] = inventory.Price;
+            dataRow[3] = inventory.Quantity;
+            dataTable.Rows.Add(dataRow);
+            dataAdapter.UpdateCommand = command.GetInsertCommand();
+            dataAdapter.Update(dataTable);
+
         }
 
         public void DeleteInventory(int id, Inventory inventory)
         {
-            throw new NotImplementedException();
+            string connectionString = Configuration["ConnectionStrings:DefaultConnection"];
+            SqlConnection sqlConnection = new SqlConnection(connectionString);
+            string query = "Select * from Inventory";
+            SqlDataAdapter dataAdapter = new SqlDataAdapter(query, sqlConnection);
+            SqlCommandBuilder command = new SqlCommandBuilder(dataAdapter);
+            DataTable dataTable = new DataTable();
+            dataAdapter.Fill(dataTable);
+            foreach (DataRow dataRow in dataTable.Rows)
+            {
+                if (Convert.ToInt32(dataRow[0]) == id)
+                {
+                    dataRow.Delete();
+                    break;
+                }
+            }
+
+            dataAdapter.UpdateCommand = command.GetDeleteCommand();
+            dataAdapter.Update(dataTable);
         }
 
         public void EditInventory(int id, Inventory inventory)
         {
-            throw new NotImplementedException();
+            string connectionString = Configuration["ConnectionStrings:DefaultConnection"];
+            SqlConnection sqlConnection = new SqlConnection(connectionString);
+            string query = "Select * from Inventory";
+            SqlDataAdapter dataAdapter = new SqlDataAdapter(query, sqlConnection);
+            SqlCommandBuilder command = new SqlCommandBuilder(dataAdapter);
+            DataTable dataTable = new DataTable();
+            dataAdapter.Fill(dataTable);
+            foreach (DataRow dataRow in dataTable.Rows)
+            {
+                if (Convert.ToInt32(dataRow[0]) == id)
+                {
+                    dataRow[1] = inventory.Name;
+                    dataRow[2] = inventory.Price;
+                    dataRow[3] = inventory.Quantity;
+                }
+            }
+
+            dataAdapter.UpdateCommand = command.GetUpdateCommand();
+            dataAdapter.Update(dataTable);
         }
 
         public Inventory GetInventory(int id)
         {
-            throw new NotImplementedException();
+            Inventory inventory = new Inventory();
+            string connectionString = Configuration["ConnectionStrings:DefaultConnection"];
+            SqlConnection sqlConnection = new SqlConnection(connectionString);
+            string query = "Select * from Inventory";
+            SqlDataAdapter dataAdapter = new SqlDataAdapter(query, sqlConnection);
+            DataTable dataTable = new DataTable();
+            dataAdapter.Fill(dataTable);
+            foreach (DataRow dataRow in dataTable.Rows)
+            {
+                if (Convert.ToInt32(dataRow[0]) == id)
+                {
+                    inventory.Id = Convert.ToInt32(dataRow[0]);
+                    inventory.Name = dataRow[1].ToString();
+                    inventory.Price = Convert.ToInt64(dataRow[2]);
+                    inventory.Quantity = Convert.ToInt32(dataRow[3]);
+                    inventory.AddedOn = Convert.ToDateTime(dataRow[4]);
+                }
+            }
+
+            return inventory;
         }
     }
 }
